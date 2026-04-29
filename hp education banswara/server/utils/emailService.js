de82fcba00785f6e736e8b861df71dbda52e3e4d
manassuthar62 <manassuthar62@gmail.com>
2026-04-29 11:27:52 +0530

    update

--- hp education banswara/server/utils/emailService.js
+++ hp education banswara/server/utils/emailService.js
@@ -2,15 +2,12 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.googlemail.com',
+  host: 'smtp-relay.brevo.com',
   port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
   }
 });
 
